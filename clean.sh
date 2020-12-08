ca457e2b7e1a039c89802965934f7a793e2182b4
Alexander Chetverkin <virtual-ins@mail.ru>
2020-12-09 02:30:20 +0300

    ftps is fixed.

--- clean.sh
+++ clean.sh
@@ -4,4 +4,5 @@ kubectl delete services --field-selector metadata.name!=kubernetes
 kubectl delete deployments --all
 kubectl delete daemonsets --all
 kubectl delete persistentvolumeclaims --all
+kubectl delete configmaps --all
 kubectl delete secrets --all
